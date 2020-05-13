f446393a3ebff5e33a647f575033da78dac0ce27
kubasikora <283418@pw.edu.pl>
2020-05-13 16:06:07 +0200

    added flask to summary search app

--- summary_search.py
+++ summary_search.py
@@ -1,22 +1,37 @@
+""" Aby uruchomić produkcyjną wersję należy skorzystać z gunicorna 
+    Uruchamiamy poleceniem:
+    gunicorn -w THREADS -b HOST:PORT summary_search:app 
+    - THREADS - liczba niezależnych workerów (wątków)
+    - HOST - host, dla localhosta ustawić 127.0.0.1
+    - PORT - port, można wybrać dowolny, polecam 5000
+"""
+
+from flask import Flask, jsonify
 from urllib.parse import urlencode
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
 opts = Options()
 opts.set_headless()
-browser = Firefox(options=opts)
+
+
+app = Flask(__name__)
 
 DUCKDUCKGO_URL = "http://duckduckgo.com/html?"
 params = {"q": "Łukasz Szumowski"}
 browser_url = "{}{}".format(DUCKDUCKGO_URL, urlencode(params))
 
-browser.get(browser_url)
-titles = browser.find_elements_by_class_name("result__title")
-snippets = browser.find_elements_by_class_name("result__snippet")
 
-results = list(zip(titles, snippets))
+@app.route("/")
+def hello():
+    browser = Firefox(options=opts)
+    browser.get(browser_url)
+    titles = browser.find_elements_by_class_name("result__title")
+    snippets = browser.find_elements_by_class_name("result__snippet")
 
-for result in results:
-    print(result[0].text)
-    print(result[1].text)
-    print("---")
+    results = list(zip(titles, snippets))
+    results = list(map(lambda x: {"title": x[0].text, "snippet": x[1].text}, results))
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run()
