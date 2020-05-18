e36012170b3a81d8479eb05b8da268bc7460c84d
kubasikora <283418@pw.edu.pl>
2020-05-18 15:56:33 +0200

    added strategy for generating queries without stopwords

--- services/QueryGeneration.py
+++ services/QueryGeneration.py
@@ -1,11 +1,33 @@
-class SingleQueryStrategy:
+import json
+import re
+
+class BasicStrategy:
+    """ Basic strategy class that provides common utilites for 
+        implemented strategies
+    """
+
+    def __init__(self, strategy_name):
+        self.strategy_name = strategy_name
+
+    def prepare_query(self, query):
+        """ Remove all non-letter signs and make all letters
+            lowercase
+
+            Arguments:
+            query -- query to be preprocessed
+        """
+
+        regex = re.compile("[^a-zA-ZąĄęĘżŻźŹćĆńŃóÓłŁśŚ ]")
+        return regex.sub("", query).lower()
+
+class SingleQueryStrategy(BasicStrategy):
     """ Implementation of single query algorithm. Returns list that contain
         only one element - the original query
     """
 
     def __init__(self):
         """ Init strategy with proper serialization name """
-        self.strategy_name = "singlequery"
+        BasicStrategy.__init__(self, "singlequery")
 
     def generate_queries(self, original_query):
         """ Create list with only one query - the original one 
@@ -15,8 +37,47 @@ class SingleQueryStrategy:
         """
         
         queries = list()
-        queries.append(original_query)
+        queries.append(self.prepare_query(original_query))
         return queries
+
+class StopwordsStrategy(BasicStrategy):
+    """ Implementation of removing stop words strategy. Returns list with query without
+        stopwords and optional original query
+    """
+
+    def __init__(self, use_original=False, pathname="./config/stopwords.config.json"):
+        """ Initialize strategy with proper serialization name
+            and load all stopwords from config file
+
+            Arguments
+            use_original -- should original query be also included
+        """
+
+        BasicStrategy.__init__(self, "stopwords")
+        self.use_original = use_original
+        with open(pathname, "r") as file:
+            self.stopwords = json.load(file)
+        
+    def is_stopword(self, word):
+        """ Check if given word is a stopword
+
+            Arguments:
+            word -- word that we want to check
+        """
+        return word in self.stopwords
+
+    def generate_queries(self, original_query):
+        queries = list()
+        query = self.prepare_query(original_query)
+        query = " ".join(filter(lambda x: not self.is_stopword(x), query.split()))
+        queries.append(query)
+
+        og_query = self.prepare_query(original_query)
+        if query != og_query and self.use_original:
+            queries.append(og_query)
+
+        return queries
+        
 
 class QueryGenerator:
     """ Service responsible for generating new queries based on a given one
